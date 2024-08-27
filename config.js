dc6cdd2a8df012feaf43ece12a774e88bb82ccbb
Alia Chughtai <alia.chughtai@gmail.com>
2024-08-27 16:01:25 +0500

    remove heading

--- config.js
+++ config.js
@@ -307,7 +307,6 @@ Here are some of their stories:
             hidden: false,
             description: `
             <span class="text-left">
-            <h2>Crosshead</h2>
 Pakistan is one of only two countries in the world where poliovirus is still endemic - the other being neighbouring Afghanistan.
 <br><br>
 The two countries share a 2,611km (1,622 miles) long border.
